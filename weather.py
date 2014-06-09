1c7d4438b674f824f9e037558abf2323053119d3
akshay <akshay_v@barunisystems.com>
2014-06-09 15:43:42 +0530

    ip api changed

--- weather.py
+++ weather.py
@@ -5,7 +5,6 @@ e-mail: anand21nanda@gmail.com
 Forked Author: Akshay Verma
 """
 
-import re
 import urllib.request
 import json
 
@@ -17,35 +16,37 @@ def get_ip():
     url = 'http://ip-api.com/json'
     req = urllib.request.Request(url, headers={'User-Agent': 'Mozilla/5.0 (Windows NT 5.1; rv:31.0) Gecko/20100101 Firefox/31.0'})
     json_html = urllib.request.urlopen(req).read()
-    jsonReply = json.loads(json_html.decode('UTF-8'))
-    ip_address = jsonReply['query']
-    return ip_address
+    json_reply = json.loads(json_html.decode('UTF-8'))
+    addr = json_reply['query']
+    return addr
 
-def get_weather(ip_address):
-    end_point = "http://api.worldweatheronline.com/free/v1/weather.ashx?" 
-    query = "key=a37fae643df77aa83d88abbc9e8e96194ab242d4&q=" + str(ip_address) + "&num_of_days=0&format=json"
+def get_weather(addr):
+    '''
+    get weather based on ip
+    '''
+    end_point = "http://api.worldweatheronline.com/free/v1/weather.ashx?"
+    query = "key=a37fae643df77aa83d88abbc9e8e96194ab242d4&q=" + str(addr) + "&num_of_days=0&format=json"
     url = end_point +  query
     json_data = urllib.request.urlopen(url).read()
-    data = json.loads(json_data.decode('UTF-8'))
-    current_weather = data['data']['current_condition'][0]
+    weather_data = json.loads(json_data.decode('UTF-8'))
+    current_weather = weather_data['data']['current_condition'][0]
     return current_weather
 
-def print_weather(data):
-    print ("""
+def print_weather(weather_data):
+    '''
+    parse and print weather
+    '''
+    print("""
     Weather : %s 
     Temperatue : %s Celsius
     Wind : %s Kmph %s 
     Humidity : %s 
     Precipitation : %s MM
-    """ % (data['weatherDesc'][0]['value'], data['temp_C'], data['windspeedKmph'], data['winddir16Point'], data['humidity'], data['precipMM']))
-
-
-def main():
-    print ("\nGetting weather information for your location...")
-    ip_address = get_ip()
-    data = get_weather(ip_address)
-    print_weather(data)
+    """ % (weather_data['weatherDesc'][0]['value'], weather_data['temp_C'], weather_data['windspeedKmph'], weather_data['winddir16Point'], weather_data['humidity'], weather_data['precipMM']))
 
 
 if __name__ == "__main__":
-    main()
+    print("\nGetting weather information for your location...")
+    addr = get_ip()
+    weather_data = get_weather(addr)
+    print_weather(weather_data)
